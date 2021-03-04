ef7c743ef0ebea4681fbcabaf51689b514723356
Shane Sutro <shanesutro@gmail.com>
2021-03-04 16:47:10 -0700

    Add learner career outcomes to seed script

--- database/seed.js
+++ database/seed.js
@@ -4,15 +4,38 @@ const { descriptionSchema } = require('./db.js');
 
 const Description = mongoose.model('Description', descriptionSchema);
 
+const getRandomPercent = () => (Math.floor(Math.random() * 100) / 100);
+
+const getFillerText = async () => {
+  const text = await axios.get('https://baconipsum.com/api/?type=meat-and-filler&paras=4&format=text');
+  return text.data;
+};
+
 const generateRecords = async () => {
   const records = [];
   for (let i = 1; i < 10; i++) {
-    // eslint-disable-next-line no-await-in-loop
-    const description = await axios.get('https://baconipsum.com/api/?type=meat-and-filler&paras=4&format=text');
     const item = {
-      course_id: i,
-      recent_views: Math.floor(Math.random() * 1000000),
-      description: description.data,
+      course_id: i, // 1 - 100
+      recent_views: Math.floor(Math.random() * 1000000), // Random number between 0 and 1 million
+      // eslint-disable-next-line no-await-in-loop
+      description: await getFillerText(), // Bacon ipsum - 4 paragraphs
+      learner_career_outcomes: [
+        {
+          icon: 'fa-map-signs',
+          pct: getRandomPercent(),
+          outcome: 'started a new career after completing these courses',
+        },
+        {
+          icon: 'fa-briefcase',
+          pct: getRandomPercent(),
+          outcome: 'got a tangible career benefit from this course',
+        },
+        {
+          icon: 'fa-money',
+          pct: getRandomPercent(),
+          outcome: 'got a pay increase or promotion',
+        },
+      ],
     };
     records.push(item);
   }
