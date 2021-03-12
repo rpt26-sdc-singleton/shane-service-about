8affa31e757b0356b1cec22fd06ce56d8325e5f7
Shane Sutro <shanesutro@gmail.com>
2021-03-11 17:40:22 -0700

    Add skills you will gain section

--- client/App.jsx
+++ client/App.jsx
@@ -5,7 +5,9 @@ class App extends React.Component {
   constructor(props) {
     super(props);
     this.state = {
-      courseInfo: {},
+      courseInfo: {
+        skills_you_will_gain: [],
+      },
     };
   }
 
@@ -24,6 +26,12 @@ class App extends React.Component {
       <div>
         <h1>About this Course</h1>
         <h3>{this.state.courseInfo.recent_views?.toLocaleString('en') || '0'} recent views</h3>
+        <div>
+          <h3>Skills You Will Gain</h3>
+          <ul>
+            {this.state.courseInfo.skills_you_will_gain?.map((skill) => <li>{skill}</li>) || ''}
+          </ul>
+        </div>
       </div>
     );
   }
