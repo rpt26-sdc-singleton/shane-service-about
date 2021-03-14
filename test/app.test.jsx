85d75acf7d9bd298be4e6d2ca7f4cbee1cb1e7ee
Shane Sutro <shanesutro@gmail.com>
2021-03-14 11:57:41 -0600

    Adding in mocks files and tests

---
+++ test/app.test.jsx
@@ -0,0 +1,14 @@
+/* eslint-disable no-undef */
+import React from 'react';
+import enzyme from 'enzyme';
+import Adapter from '@wojtekmaj/enzyme-adapter-react-17';
+import App from '../client/App';
+
+enzyme.configure({ adapter: new Adapter() });
+
+describe('Should render the About component', () => {
+  test('Should open without crashing', () => {
+    const wrapper = enzyme.render(<App />);
+    console.log(wrapper.children);
+  });
+});
