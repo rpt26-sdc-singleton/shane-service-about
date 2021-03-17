555b65ef16f0a55b7ee3abb4498816d1172c3774
Shane Sutro <shanesutro@gmail.com>
2021-03-17 07:47:48 -0600

    Styling Outcomes section

--- client/components/meta/Outcomes.jsx
+++ client/components/meta/Outcomes.jsx
@@ -4,8 +4,19 @@ const Outcomes = (props) => {
   const { state } = props;
   return (
     <div id="outcomes">
-      <div className="placeholder-circle" />
-      <h2>Learner Career Outcomes</h2>
+      <div className="outcomes-title-set">
+        <div className="circle-outline" />
+        <h2>Learner Career Outcomes</h2>
+      </div>
+      {state.learner_career_outcomes.map((outcome) => (
+        <div className="outcome-set">
+          <div className="icon-background placeholder-circle">
+            <div className="outcome-icon" />
+          </div>
+          <h2>{`${Math.round(outcome.pct * 100)}%`}</h2>
+          <h3>{outcome.outcome}</h3>
+        </div>
+      ))}
     </div>
   );
 };
